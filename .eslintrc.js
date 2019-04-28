68f616c93ac3a21812b46a1ce5b30973f422ce85
Chris Thoburn <cthoburn@linkedin.com>
2019-04-28 01:05:27 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,79 @@
+module.exports = {
+  parser: "babel-eslint",
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  extends: ["eslint:recommended", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+
+    "no-unused-vars": [
+      "error",
+      {
+        args: "none"
+      }
+    ],
+
+    "no-cond-assign": ["error", "except-parens"],
+    eqeqeq: "error",
+    "no-eval": "error",
+    "new-cap": [
+      "error",
+      {
+        capIsNew: false
+      }
+    ],
+    "no-caller": "error",
+    "no-irregular-whitespace": "error",
+    "no-undef": "error",
+    "no-eq-null": "error"
+  },
+  globals: {
+    Map: true,
+    WeakMap: true
+  },
+  env: {
+    browser: true,
+    node: false
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        ".eslintrc.js",
+        ".prettierrc.js",
+        "packages/*/ember-cli-build.js",
+        "packages/*/index.js",
+        "packages/*/testem.js",
+        "packages/*/bin/**",
+        "packages/*/blueprints/*/index.js",
+        "packages/*/blueprints/*.js",
+        "packages/*/config/**/*.js",
+        "packages/*/lib/**/*.js",
+        "packages/*/tests/dummy/config/**/*.js"
+      ],
+      excludedFiles: ["packages/*/addon/**/index.js"],
+      parserOptions: {
+        sourceType: "script",
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true,
+        es6: true
+      },
+      plugins: ["node"],
+      // eslint-disable-next-line node/no-unpublished-require
+      rules: Object.assign(
+        {},
+        require("eslint-plugin-node").configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+        }
+      )
+    }
+  ]
+};
---
+++ packages/warp-drive/README.md
@@ -0,0 +1,26 @@
+# warp-drive
+
+Not Even Alpha. Avoid At All Costs
+
+## Compatibility
+
+- Ember.js v3.8 or above
+- Ember CLI v3.8 or above
+
+## Installation
+
+```
+ember install warp-drive
+```
+
+## Usage
+
+Don't.
+
+## Contributing
+
+See the [Contributing](CONTRIBUTING.md) guide for details.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
